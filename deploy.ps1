1e985dd0ff315a9048b7d752b95cd4b30d1db26b
Dmitry Kostenko <dk@sitecore.net>
2017-02-23 02:45:11 +0100

    Add timestamp to the URL to work around caching

--- deploy.ps1
+++ deploy.ps1
@@ -1,3 +1,5 @@
-New-AzureRMResourceGroup -ResourceGroupName dk-test-3 -Location "North Europe"
+$TS=[System.DateTime]::UtcNow.ToString('hhmmss')
 
-New-AzureRMResourceGroupDeployment -Name "test" -ResourceGroupName dk-test-3 -TemplateUri https://raw.githubusercontent.com/sc-dmitrykostenko/arm-linked-templates/master/azuredeploy.json -TemplateParameterFile azuredeploy.parameters.json
+New-AzureRMResourceGroup -ResourceGroupName dk-test-3 -Location "North Europe" -Force
+
+New-AzureRMResourceGroupDeployment -Name "test" -ResourceGroupName dk-test-3 -TemplateUri https://raw.githubusercontent.com/sc-dmitrykostenko/arm-linked-templates/master/azuredeploy.json?ts=$TS -TemplateParameterFile azuredeploy.parameters.json
