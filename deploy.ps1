621fc5bc4a341ea4878a662d278b899c783239c1
Dmitry Kostenko <dk@sitecore.net>
2017-02-23 02:38:30 +0100

    Fixed parameters and the deployment script

--- deploy.ps1
+++ deploy.ps1
@@ -1,3 +1,3 @@
 New-AzureRMResourceGroup -ResourceGroupName dk-test-3 -Location "North Europe"
 
-New-AzureRMResourceGroupDeployment -Name "test" -ResourceGroupName dk-test-3 -TemplateUri https://raw.githubusercontent.com/sc-dmitrykostenko/arm-linked-templates/master/3master-template.json -TemplateParameterFile 3master-parameters.json
+New-AzureRMResourceGroupDeployment -Name "test" -ResourceGroupName dk-test-3 -TemplateUri https://raw.githubusercontent.com/sc-dmitrykostenko/arm-linked-templates/master/azuredeploy.json -TemplateParameterFile azuredeploy.parameters.json
